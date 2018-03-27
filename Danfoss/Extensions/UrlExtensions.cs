c3b8d18b94c786564da7a7d14a1180aa45dd594e
欧阳武 <oyangwu@gmail.com>
2018-03-27 13:41:10 +0800

    dd

--- Danfoss/Extensions/UrlExtensions.cs
+++ Danfoss/Extensions/UrlExtensions.cs
@@ -11,18 +11,18 @@ namespace Danfoss.Extensions
         public static string Img(this UrlHelper helper, string url, string folder = null)
         {
             if (string.IsNullOrEmpty(folder))
-                return helper.RequestContext.HttpContext.Request.Url.GetLeftPart(UriPartial.Authority) + string.Format("/content/images/{0}", url);
-            return helper.RequestContext.HttpContext.Request.Url.GetLeftPart(UriPartial.Authority) + string.Format("/content/{1}/{0}", url, folder);
+                return string.Format("/content/images/{0}", url);
+            return string.Format("/content/{1}/{0}", url, folder);
         }
 
         public static string Solution(this UrlHelper helper, int id)
         {
-            return helper.RequestContext.HttpContext.Request.Url.GetLeftPart(UriPartial.Authority) + string.Format("/solution/detail/{0}", id);
+            return string.Format("/solution/detail/{0}", id);
         }
 
         public static string Download(this UrlHelper helper, string file)
         {
-            return helper.RequestContext.HttpContext.Request.Url.GetLeftPart(UriPartial.Authority) + string.Format("/content/download/{0}", file);
+            return  string.Format("/content/download/{0}", file);
 
             //return string.Format("/content/download/{0}", helper.RequestContext.HttpContext.Server.UrlEncode(file));
         }
